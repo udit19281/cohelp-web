a78d33e6c6c6d6b23b802bdd1d8f468e773dd4b0
vidhi448 <55681509+vidhi448@users.noreply.github.com>
2021-05-10 17:24:52 +0530

    bloodPlasma.csv and foodSupport.csv updated

--- load_data.sql
+++ load_data.sql
@@ -1,4 +1,13 @@
 use cohelpdb;
+
 LOAD DATA LOCAL INFILE 'C:/Users/Dell/Desktop/NGO/web/data2/ambulance.csv' 
 INTO TABLE ambulance FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
 IGNORE 1 ROWS (name, contact_number, address, link, status,contact_person);
+
+LOAD DATA LOCAL INFILE 'C:/Users/Dell/Desktop/NGO/web/data2/foodSupport.csv' 
+INTO TABLE FoodSupport FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS (name, contact_number, address, contact_person, status);
+
+LOAD DATA LOCAL INFILE 'C:/Users/Dell/Desktop/NGO/web/data2/bloodPlasma.csv' 
+INTO TABLE BloodPlasma FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS (name, contact_number, address, contact_person, medical_conditions, availibility_status, link);
--- main/models.py
+++ main/models.py
@@ -23,7 +23,9 @@ class FoodSupport(models.Model):
 
    name = models.CharField(max_length = 50)
    contact_number = models.IntegerField()
-   address = models.CharField(max_length = 70)
+   contact_person = models.CharField(max_length =100,default="NA")
+   address = models.CharField(max_length = 70,default="NA")
+   status=models.CharField(max_length = 100,default="NA")
    no_of_meals = models.IntegerField(default=0)
    verified = models.BooleanField(default=False)
 
@@ -49,10 +51,10 @@ class BloodPlasma(models.Model):
    name = models.CharField(max_length = 50)
    contact_number = models.IntegerField()
    contact_person = models.CharField(max_length =100,default="NA")
-   blood_group = models.CharField(max_length = 4)
-   address = models.CharField(max_length = 70)
-   medical_conditions = models.CharField(max_length = 70)
-   required_bloodgroup = models.CharField(max_length = 4)
+   address = models.CharField(max_length = 70,default="NA")
+   link=models.URLField(max_length = 200,default="http://")
+   medical_conditions = models.CharField(max_length = 70,default="NA")
+   availibility_status= models.CharField(max_length = 100,default="NA")
    verified = models.BooleanField(default=False)
    
    class Meta:
