7c0d5fbc5a27ab12d7b7b3ac7d67f67b57927c7d
udit19281 <udit19281@iiitd.ac.in>
2021-05-31 04:03:35 +0530

    deploy

---
+++ staticfiles/js/vali-email.js
@@ -0,0 +1,58 @@
+window.onload=function() {
+    const emailfield=document.getElementById("emailf");
+    const emailfeed=document.getElementById("emailfeedbackarea");
+    const usernamefeed=document.getElementById("usernamefeedbackarea");
+    const showps=document.getElementById("show");
+    const ps=document.getElementById("passwordf");
+    const username=document.getElementById("usernamef");
+    const btn=document.getElementById("submit");
+    showps.addEventListener("click",()=>{
+        // console.log("Clicked");
+        if(showps.textContent==="Show"){
+            showps.textContent="Hide";
+            ps.setAttribute("type","text");
+            showps.style.color="red";
+        }
+        else{
+            showps.textContent="Show";
+            showps.style.color="blue";
+            ps.setAttribute("type","password");
+        }
+    });
+    username.addEventListener("keyup",(e)=>{
+        const value = e.target.value;
+        usernamefeed.classList.remove("is-valid");
+        usernamefeed.style.display="none";
+        if(value.length>0){
+            fetch('/auth/valid-username/',{
+                method: "POST",
+                body: JSON.stringify({username:value}),})
+                .then((res)=>res.json()).then((data)=>{
+                    console.log(data);
+                    if(data.username_error){
+                        usernamefeed.classList.add("is-valid");
+                        usernamefeed.style.display="block";
+                        usernamefeed.innerHTML=`<p>${data.username_error}</p>`;
+                    }
+                })
+            }
+        })
+    emailfield.addEventListener("keyup",(e)=>{
+        const val=e.target.value;
+        emailfeed.classList.remove("is-valid");
+        emailfeed.style.display="none";
+        if(val.length>0){
+            fetch('/auth/valid-email/',{
+            method: 'POST',
+            body:JSON.stringify({email:val}),})
+            .then((res)=>res.json()).then((data)=>{
+                console.log(data);
+                if(data.email_error){
+                    emailfeed.classList.add("is-valid");
+                    emailfeed.style.display="block";
+                    emailfeed.innerHTML=`<p>${data.email_error}</p>`;
+                }
+            })
+    }
+        })
+}
